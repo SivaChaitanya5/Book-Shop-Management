bcdf48f1f1851c812070d61a9646e584ea6a09a1
Chaitanya Atchuta <Chaitanya Atchuta@DESKTOP-18HF8DG>
2019-04-26 05:05:00 +0530

    Corrected the Errors

--- src/main/java/main/Item.java
+++ src/main/java/main/Item.java
@@ -5,7 +5,8 @@ public class Item{
     public  void addItem(String itemName,int mrpItem){
         //write your code here
     	//-------------------
-
+    	setItemName(itemName);
+    	setMrp(mrpItem);
     }
 	public String getItemName() {
 		return itemName;
